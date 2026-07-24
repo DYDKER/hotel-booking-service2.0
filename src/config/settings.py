297605ed32c15ecdb396e9d712382ebb82b4bd5d
sultan <utegenov.sultan@inbox.ru>
2026-07-24 21:23:30 +0400

    chore: load django settings from environment

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from config.settings_env import settings
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kh@7=&phmj3&!qc$htexk)0q0_&yowk04hvo7e9+x9a_0v411c'
+SECRET_KEY = settings.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = settings.debug
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = settings.allowed_hosts
 
 
 # Application definition
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': settings.postgres_db,
+        'USER': settings.postgres_user,
+        'PASSWORD': settings.postgres_password,
+        'HOST': settings.postgres_host,
+        'PORT': settings.postgres_port,
     }
 }
 
