c5a2dbb05e613d471f8bb668be91ca2691e4b614
sultan <utegenov.sultan@inbox.ru>
2026-07-24 23:18:30 +0400

    feat: add room and booking models

---
+++ src/hotels/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Room(models.Model):
+    description = models.TextField()
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Booking(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    date_start = models.DateField()
+    date_end = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
